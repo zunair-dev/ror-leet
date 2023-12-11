094e89a40118de350dd9fc76bc7b1fca8b2bb4b4
Zunair <zunair.dev@gmail.com>
2023-12-11 22:47:01 +0500

    fixed the 2nd problem

--- ruby/2. remove_element.rb
+++ ruby/2. remove_element.rb
@@ -2,5 +2,14 @@
 # @param {Integer} val
 # @return {Integer}
 def remove_element(nums, val)
-    
+     k = 0  # Variable to track the number of elements not equal to val
+
+    nums.each do |num|
+        if num != val
+            nums[k] = num
+            k += 1
+        end
+    end
+
+    return k
 end
